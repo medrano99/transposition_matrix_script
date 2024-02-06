f49061dfbb0b86c7ebd8508017d8ec479471396f
Alexander Medrano <33732034+medrano99@users.noreply.github.com>
2024-02-06 18:35:06 -0500

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 	empleando un hilo POSIX.
 	Para compilar el programa, emplee el comando 
 	
-	$ gcc -pthread transposicion.c -o transposicion
+	$ gcc -pthread main.c -o main
 
 	Para ejecutarlo y recolectar los datos, emplee el comando
 
